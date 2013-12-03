ff821cce12a13fdeca4fc7e447c657ab6b906f7a
Elliott Ison <elliott-ison@berkeley.edu>
2013-12-02 18:55:14 -0800

    SpatialGrids are hell
    no idea if this works

--- SpatialGrid.h
+++ SpatialGrid.h
@@ -6,11 +6,17 @@
 class SpatialGrid
 {
 public:
-	SpatialGrid(float s, float h);//establish grid with h box lengths and s length sides of a huge cube grid thingy at point 0,0,0
-	void addParticle(Point3D p); //add particle to list of particles in box
+	SpatialGrid(int s, float h);//establish grid with h box lengths of s*x side lengths of a huge cube grid thingy at point 0,0,0
+	void addParticle(Particle p); //add particle to list of particles in box
+	void updateBoxes(); //when particles move, they might change boxes, so we need to update boxes
+	std::vector<Particle> getNeighbors(Particle p); //get the neighbor particles in the box and in the surrounding boxes
 	
 private:
 	std::vector<std::vector<std::vector<std::vector<Particle> > > > grid;
+	float sideLength;
+	float boxLength;
+	int numEdgeBoxes;
+	Point3D start;
 
 };
 
