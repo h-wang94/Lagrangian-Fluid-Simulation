2ab4f3262d281d5fb4b9d64791d980e09f3b8010
miss-ng <kehau1an1@hotmail.com>
2013-12-12 01:02:23 -0800

    wrong file
    i'm tired

--- mesh.cpp
+++ mesh.cpp
@@ -26,7 +26,7 @@ Mesh::Mesh(int width, int height, int depth) {
 	float cubeD = B * 2 / (float) depth;
 	int i = 0, j = 0, k = 0;
 	for (float z = B; z >= -B; z-=cubeD) {
-		for (float y = -T; y <= T; y+=cubeH) {
+		for (float y = T; y >= -T; y-=cubeH) {
 			for (float x = -1.0; x <= 1.0; x+=cubeW) {
 				setVertexAt(i, j, k, CubeVertex(Water(Point3D(x, y, z))));
 				i++;
@@ -67,23 +67,15 @@ vector<vector<Point3D> > Mesh::marchingCubes() {
 		for (int y = 0; y < height - 1; y++) {
 			for (int x = 0; x < width - 1; x++) {
 				cv.resize(0); //reset the vertices
-				cv.push_back(getVertexAt(x, y, z));
-				cv.push_back(getVertexAt(x+1, y, z));
-				cv.push_back(getVertexAt(x+1, y, z+1));
-				cv.push_back(getVertexAt(x, y, z+1));
-				cv.push_back(getVertexAt(x, y+1, z));
-				cv.push_back(getVertexAt(x+1, y+1, z));
-				cv.push_back(getVertexAt(x+1, y+1, z+1));
-				cv.push_back(getVertexAt(x, y+1, z+1));
 
-				/*cv.push_back(getVertexAt(x, y+1, z+1)); //Corner 0, according to Bourke's picture
+				cv.push_back(getVertexAt(x, y+1, z+1)); //Corner 0, according to Bourke's picture
 				cv.push_back(getVertexAt(x+1, y+1, z+1));
 				cv.push_back(getVertexAt(x+1, y+1, z));
 				cv.push_back(getVertexAt(x, y+1, z));
 				cv.push_back(getVertexAt(x, y, z+1));
 				cv.push_back(getVertexAt(x+1, y, z+1));
 				cv.push_back(getVertexAt(x+1, y, z));
-				cv.push_back(getVertexAt(x, y, z));*/
+				cv.push_back(getVertexAt(x, y, z));
 				c.setVertices(cv);
 				triangles.push_back(c.getTriangles(0.5));
 			}
